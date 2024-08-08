148c4ef9b9fbb848bbe36d00fd31dcfe691684b4
Andrew Gremlich <agremlich@anonyome.com>
2024-08-07 21:11:08 -0600

    experimenting with render

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,31 @@
-import "./style.css";
+// import "./style.css";
+
+import { primitives } from "@jscad/modeling";
+import {
+	cameras,
+	controls,
+	drawCommands,
+	entitiesFromSolids,
+	prepareRender,
+} from "@jscad/regl-renderer";
 
 import { hello } from "@/control";
 
 hello();
+
+const app = document.getElementById("app");
+
+if (app === null) {
+	throw new Error("Element with id 'app' not found");
+}
+
+prepareRender({ glOptions: { container: app } });
+
+const { cube } = primitives;
+
+const main = () => {
+	const myCube = cube({ size: 10 });
+	return myCube;
+};
+
+console.log(main());
