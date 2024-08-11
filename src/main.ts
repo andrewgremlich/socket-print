b81dc0eaa94909c429e29cc9070cf20b187893ba
Andrew Gremlich <agremlich@anonyome.com>
2024-08-10 22:02:35 -0600

    Super basic three js settings

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./style.css";
 
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -10,6 +11,7 @@ const camera = new THREE.PerspectiveCamera(
 	1000,
 );
 const renderer = new THREE.WebGLRenderer();
+const controls = new OrbitControls(camera, renderer.domElement);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
@@ -19,14 +21,16 @@ document.body.appendChild(renderer.domElement);
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
+const gridHelper = new THREE.GridHelper(10, 10);
 
 scene.add(cube);
+scene.add(gridHelper);
 
-camera.position.z = 5;
+camera.position.set(5, 2, 5);
 
 function animate() {
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
+	// cube.rotation.x += 0.01;
+	// cube.rotation.y += 0.01;
+	controls.update();
 	renderer.render(scene, camera);
 }
