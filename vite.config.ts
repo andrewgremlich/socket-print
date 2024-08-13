457b2355e63e7902a22ae185f127cfa1973f951f
Andrew Gremlich <agremlich@anonyome.com>
2024-08-12 20:40:59 -0600

    Adding vite PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,31 @@
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()]
-})
+	plugins: [
+		tsconfigPaths(),
+		VitePWA({
+			registerType: "autoUpdate",
+			// includeAssets: ["/favicon.ico", "/robots.txt", "/safari-pinned-tab.svg"],
+			manifest: {
+				name: "Socket Print",
+				short_name: "SP",
+				description: "A simple CAD software to design socket prosthesis.",
+				theme_color: "#ffffff",
+				// icons: [
+				//   {
+				//     src: "/android-chrome-192x192.png",
+				//     sizes: "192x192",
+				//     type: "image/png",
+				//   },
+				//   {
+				//     src: "/android-chrome-512x512.png",
+				//     sizes: "512x512",
+				//     type: "image/png",
+				//   },
+				// ],
+			},
+		}),
+	],
+});
