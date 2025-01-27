97855bb759bb22969ea967cdf640b1e52d7ba64e
Andrew Gremlich <andrew.gremlich@gmail.com>
2025-01-27 11:43:23 -0700

    Update storeFunctions.ts
    removing isValidIpAddress

--- src/utils/storeFunctions.ts
+++ src/utils/storeFunctions.ts
@@ -1,14 +1,6 @@
 import type { ProvelPrintApp } from "@/global";
 import { appForm } from "./htmlElements";
 
-export function isValidIpAddress(ipAddress: string) {
-	const splitValue = ipAddress.split(".");
-	return (
-		splitValue.length === 4 &&
-		splitValue.every((v) => v !== "" && !Number.isNaN(Number(v)))
-	);
-}
-
 export function loadMainDataForm() {
 	const store = window.provelPrintStore;
 	const storeForm = new FormData(appForm);
