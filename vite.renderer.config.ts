4078f3bb8f78d4f60cdcee88e7f324bfc69b188d
andrewgremlich <andrew.gremlich@gmail.com>
2025-02-01 19:28:36 -0700

    putting pwa back in

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,5 +1,6 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import topLevelAwait from "vite-plugin-top-level-await";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -16,5 +17,32 @@ export default defineConfig(() => ({
 			},
 		},
 	},
-	plugins: [tsconfigPaths(), topLevelAwait()],
+	plugins: [
+		tsconfigPaths(),
+		topLevelAwait(),
+		VitePWA({
+			registerType: "autoUpdate",
+			// includeAssets: ["/favicon.ico", "/robots.txt", "/safari-pinned-tab.svg"],
+			manifest: {
+				name: "Provel Print",
+				display: "standalone",
+				short_name: "PP",
+				description:
+					"A simple CAD software to join socket prosthesis with distal cups.",
+				theme_color: "#ffffff",
+				// icons: [
+				//   {
+				//     src: "/android-chrome-192x192.png",
+				//     sizes: "192x192",
+				//     type: "image/png",
+				//   },
+				//   {
+				//     src: "/android-chrome-512x512.png",
+				//     sizes: "512x512",
+				//     type: "image/png",
+				//   },
+				// ],
+			},
+		}),
+	],
 }));
