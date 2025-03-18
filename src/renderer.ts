98446170e5471600759226a74999bbe77fbc9d46
andrewgremlich <andrew.gremlich@gmail.com>
2025-03-18 17:16:39 -0600

    don't deploy tauri stuff.

--- src/renderer.ts
+++ src/renderer.ts
@@ -9,7 +9,7 @@ import { Lighting } from "@/classes/Lighting";
 import { MergeCup } from "@/classes/MergeCup";
 import { MergeGeometries } from "@/classes/MergeGeometries";
 import { Socket } from "@/classes/Socket";
-import { invoke } from "@tauri-apps/api/core";
+// import { invoke } from "@tauri-apps/api/core";
 import { ceil } from "mathjs";
 
 import { adjustForShrinkAndOffset } from "@/3d/adjustForShrinkAndOffset";
@@ -173,22 +173,22 @@ export async function slicingAction(sendToFile: boolean) {
 
 generateGCodeButton.addEventListener("click", async () => {
 	try {
-		// await slicingAction(true);
-		const hello = await invoke("greet", { name: "Andrew" });
-		console.log(hello);
+		await slicingAction(true);
+		// const hello = await invoke("greet", { name: "Andrew" });
+		// console.log(hello);
 
-		mergeGeometries.updateMatrixWorld();
-		const testSlicer = await invoke("slicer", {
-			positions: Array.from(
-				mergeGeometries.mesh.geometry.attributes.position.array,
-			),
-			center: [
-				mergeGeometries.center.x,
-				mergeGeometries.center.y,
-				mergeGeometries.center.z,
-			],
-		});
-		console.log(testSlicer);
+		// mergeGeometries.updateMatrixWorld();
+		// const testSlicer = await invoke("slicer", {
+		// 	positions: Array.from(
+		// 		mergeGeometries.mesh.geometry.attributes.position.array,
+		// 	),
+		// 	center: [
+		// 		mergeGeometries.center.x,
+		// 		mergeGeometries.center.y,
+		// 		mergeGeometries.center.z,
+		// 	],
+		// });
+		// console.log(testSlicer);
 	} catch (error) {
 		console.error("Error invoking Rust function:", error);
 	}
