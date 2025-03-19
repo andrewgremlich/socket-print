497dd9af3c63bd6025f5efdb6759023be37f83db
andrewgremlich <andrew.gremlich@gmail.com>
2025-03-18 21:11:42 -0600

    trying to find a better algorithm

--- src-tauri/src/slicer/mod.rs
+++ src-tauri/src/slicer/mod.rs
@@ -20,18 +20,11 @@ fn triangulate(input: Vec<f32>) -> Vec<Triangle> {
     triangled
 }
 
-fn find_height(positions: &Vec<Triangle>) -> f32 {
-    let mut max_height = f32::MIN;
+fn find_height(positions: &Triangle) -> (f32, f32) {
+    let max_height = positions.p1.y.max(positions.p2.y).max(positions.p3.y);
+    let min_height = positions.p1.y.min(positions.p2.y).min(positions.p3.y);
 
-    for triangle in positions {
-        let height = triangle.p1.y.max(triangle.p2.y).max(triangle.p3.y);
-
-        if height > max_height {
-            max_height = height;
-        }
-    }
-
-    max_height
+    (min_height, max_height)
 }
 
 #[tauri::command]
@@ -43,15 +36,27 @@ pub fn slicer(positions: Vec<f32>, center: Vec<f32>) -> Vec<Vec<f32>> {
     let height_change = layer_height / segments;
     let triangled: Vec<Triangle> = triangulate(positions.clone());
     let mut intersections: Vec<Vec<f32>> = vec![];
-    let mut raycaster = RayCaster::new(
-        Vec3::new(center[0], 0.0, center[2]),
-        Vec3::new(0.0, 0.0, -1.0),
-    );
-    let height = find_height(&triangled);
+
+    // let mut raycasters: Vec<RayCaster> = vec![];
+    // let mut exceeded_height = false;
+    // let mut height = 0.0;
+
+    // while !exceeded_height {
+    //     let mut raycaster = RayCaster::new(
+    //         Vec3::new(center[0], height, center[2]),
+    //         Vec3::new(0.0, height, -1.0),
+    //     );
+    // }
 
     for triangle in triangled {
         let mut above_height = false;
-
+        let triangle_height = find_height(&triangle);
+        let mut raycaster = RayCaster::new(
+            Vec3::new(center[0], triangle_height.0, center[2]),
+            Vec3::new(0.0, 0.0, -1.0),
+        );
+        // https://crates.io/crates/bvh
+        // https://chatgpt.com/c/67da34ed-15ac-800d-bbba-50f58151d318
         while !above_height {
             if let Some(intersection) = triangle.ray_intersection(&raycaster) {
                 intersections.push(vec![
@@ -65,11 +70,7 @@ pub fn slicer(positions: Vec<f32>, center: Vec<f32>) -> Vec<Vec<f32>> {
 
             raycaster.ray_rotate(angle_increment, height_change);
 
-            if raycaster.origin.y > height {
-                raycaster.reset_raycaster(
-                    Vec3::new(center[0], 0.0, center[2]),
-                    Vec3::new(0.0, 0.0, -1.0),
-                );
+            if raycaster.origin.y > triangle_height.1 {
                 above_height = true;
             }
         }
