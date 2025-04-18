2d7d093aceb179130162fd484898375d23ab8292
andrewgremlich <andrew.gremlich@gmail.com>
2025-04-17 20:21:52 -0600

    removing deno option

--- src/renderer.ts
+++ src/renderer.ts
@@ -37,20 +37,6 @@ if (!window.Worker) {
 	throw new Error("Web Worker not supported");
 }
 
-fetch("http://localhost:8080/")
-	.then((response) => {
-		if (!response.ok) {
-			throw new Error("Network response was not ok");
-		}
-		return response.json();
-	})
-	.then((data) => {
-		console.log(data);
-	})
-	.catch((error) => {
-		console.error("There was a problem with the fetch operation:", error);
-	});
-
 const app = new Application();
 
 const lighting = new Lighting();
