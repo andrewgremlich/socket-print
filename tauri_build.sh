48414628e6617754e579f1771f00c5d5fb1f806e
andrewgremlich <andrew.gremlich@gmail.com>
2025-04-17 21:33:54 -0600

    temporarily undoing tauri github action

---
+++ tauri_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Export Apple ID and Apple Password as environment variables
+export APPLE_ID="andrew.gremlich@icloud.com"
+export APPLE_PASSWORD="65EcTe#TD@fmBqn#"
+
+export TAURI_SIGNING_PRIVATE_KEY="/Users/andrewgremlich/.tauri/myapp.key"
+# optionally also add a password
+export TAURI_SIGNING_PRIVATE_KEY_PASSWORD="Retrial-Irritably-Fondling2"
+
+echo "Environment variables APPLE_ID and APPLE_PASSWORD have been set."
