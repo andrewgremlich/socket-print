bffb9592b5b3383e523b2d3053eea0a070c2dee6
andrewgremlich <andrew.gremlich@gmail.com>
2025-07-06 23:00:35 -0600

    additional styling feature

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // NOTE: This only works in HTTPS
-const cacheName = "v7";
+const cacheName = "v8";
 
 const addResourcesToCache = async (resources) => {
 	const cache = await caches.open(cacheName);
