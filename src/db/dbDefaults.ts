eb4b2074c5b59f8176932533b51942c08d274a06
andrewgremlich <andrew.gremlich@gmail.com>
2025-08-12 18:52:52 -0600

    more feedback

--- src/db/dbDefaults.ts
+++ src/db/dbDefaults.ts
@@ -10,7 +10,7 @@ const materialProfileDefaults: Omit<MaterialProfile, "id"> = {
 	nozzleTemp: 200,
 	cupTemp: 130,
 	shrinkFactor: 2.6,
-	outputFactor: 20,
+	outputFactor: 15,
 	secondsPerLayer: 8,
 };
 
