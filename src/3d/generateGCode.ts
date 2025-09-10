7e7614f2e96492d42eef013ca5a9ebf18cc2d7cb
andrewgremlich <andrew.gremlich@gmail.com>
2025-09-10 17:58:27 -0600

    added m106 command

--- src/3d/generateGCode.ts
+++ src/3d/generateGCode.ts
@@ -80,6 +80,7 @@ export async function generateGCode(
 		"T0 ;select tool 0",
 
 		";## Set temperatures ##",
+		"M106 P0 S0 H3 L0.15 X0.25 T20:40",
 		"M568 P0 S200 ; set temperature for barrel to 200;",
 		"M140 P1 S160  ; set cup heater temperature to 160 and continue",
 
