987f9ee36c839f3cde9522daba1b20390c4212ce
andrewgremlich <andrew.gremlich@gmail.com>
2026-01-08 13:55:24 -0700

    remove unused function

--- src/3d/sendGCodeFile.ts
+++ src/3d/sendGCodeFile.ts
@@ -14,22 +14,6 @@ type ModelInformation = {
 	result: number;
 };
 
-export async function setPrinterIp(ipAddress: string) {
-	const response = await fetch("http://127.0.0.1:8724/printer_ip", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ ip: ipAddress }),
-	});
-
-	if (!response.ok) {
-		throw new Error("Network response was not ok");
-	}
-
-	return await response.json();
-}
-
 // DOCS: https://github.com/Duet3D/RepRapFirmware/wiki/HTTP-requests
 export async function connectToPrinter(ipAddress: string) {
 	const password = "";
