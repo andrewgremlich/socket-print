e5484b4a0a3bbe7fe13fbbc56d489611a9fb91d3
andrewgremlich <andrew.gremlich@gmail.com>
2026-01-13 15:33:57 -0700

    nozzle size just 1

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,4 +3,4 @@ import { pi } from "mathjs";
 // Rotation constants
 export const QUARTER_TURN = pi / 2;
 export const HALF_TURN = pi;
-export const NOZZLE_SIZE_OFFSET_FACTOR = 2;
+export const NOZZLE_SIZE_OFFSET_FACTOR = 1;
